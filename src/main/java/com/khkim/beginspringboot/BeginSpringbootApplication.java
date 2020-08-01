0a535073cb19a3906d866e785de31bb85492b303
khkim <khkim@localhost.localdomain>
2020-08-01 23:00:47 +0900

    Initial commit

---
+++ src/main/java/com/khkim/beginspringboot/BeginSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.khkim.beginspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeginSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeginSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/khkim/beginspringboot/BeginSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khkim.beginspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeginSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
